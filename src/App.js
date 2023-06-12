3ccd5f91db532872ed01e97c04948915bdca639b
Vladislav <102317010+Lojkuc@users.noreply.github.com>
2023-06-12 19:11:07 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Exchanges from "./Exchanges";
+import "./style/App.scss";
+function App() {
+  return (
+    <div className="App">
+      <Exchanges exchanges="binance" />
+      <Exchanges exchanges="kucoin" />
+      <Exchanges exchanges="huobi" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Exchanges.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import "./style/App.scss";
+function Exchanges({ title, exchanges }) {
+  const [price, setPrice] = useState(null);
+  useEffect(() => {
+    // Функция для получения цены с биржи Binance
+    const fetchPrice = async () => {
+      try {
+        const response = await fetch(
+          `http://server-exchanges.onrender.com/coinPrice`
+        );
+        const data = await response.json();
+        setPrice(data[`${exchanges}`].price);
+      } catch (error) {
+        console.error("Failed to fetch Binance price:", error);
+      }
+    };
+    setInterval(() => {
+      fetchPrice();
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="Exchanges">
+      <h2 className="Exchanges-name">{exchanges}</h2>
+      <p className="Exchanges-price">{price}$</p>
+    </div>
+  );
+}
+
+export default Exchanges;
